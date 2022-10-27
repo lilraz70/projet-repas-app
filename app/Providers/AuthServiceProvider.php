da8107551b0ee984f470b1f0b29c6c5b9a078ca8
lilraz70 <mahamadou.abdoulrazak@yahoo.com>
2022-10-27 15:56:32 +0000

    pc change

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -25,6 +25,7 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
+        
             // cree un gate pour autoriser uniquement les administrateurs
 
         Gate::define("administrateur",function(User $user){
